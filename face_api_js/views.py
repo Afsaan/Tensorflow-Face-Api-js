bf9589f2453489d24feb2a282c0929a1472e505e
afsan <khanafsaan11@gmail.com>
2020-10-23 09:46:18 +0530

    added fucntion to render more pages

--- face_api_js/views.py
+++ face_api_js/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def mtcnn(request):
+    return render(request, 'mtcnn.html')
+
+def tiny(request):
+    return render(request, 'tiny.html')
+
+def ssd(request):
+    return render(request, 'mobilenet.html')
