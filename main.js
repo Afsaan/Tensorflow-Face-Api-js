65227e5fa57fa709dfdfe2613cd5d72025cb8a75
afsan <khanafsaan11@gmail.com>
2020-10-23 08:11:54 +0530

    js file for loading the model and detection

---
+++ main.js
@@ -0,0 +1,31 @@
+const video = document.getElementById('video')
+
+
+Promise.all(
+    [
+        faceapi.loadMtcnnModel('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+    ]
+
+).then(start_video)
+
+function start_video(){
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+// start_video()
+video.addEventListener('play', () =>
+{
+    setInterval(async () => {
+       const detections = await faceapi.detectAllFaces(video, new faceapi.MtcnnOptions()).withFaceLandmarks() 
+       console.log(detections.length  )
+        point_36_x = detections[0]['landmarks']['_positions'][36]['_x']
+        point_36_y = detections[0]['landmarks']['_positions'][36]['_y']
+       console.log(point_36_x )
+       console.log(point_36_y )
+        
+})})
